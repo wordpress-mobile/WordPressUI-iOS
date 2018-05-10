29ba0f7a6b16aa32a2071323f9c8b13146936247
Jorge Leandro Perez <jorge@lantean.co>
2018-05-10 12:54:04 -0300

    UIAlertController: Nuking troublesome method

--- WordPressUI/Extensions/UIAlertController+Helpers.swift
+++ WordPressUI/Extensions/UIAlertController+Helpers.swift
@@ -20,22 +20,4 @@ extension UIAlertController {
 
         return action
     }
-
-    @objc public func presentFromRootViewController() {
-        // Note:
-        // This method is required because the presenter ViewController must be visible, and we've got several
-        // flows in which the VC that triggers the alert, might not be visible anymore.
-        //
-        guard let rootViewController = UIApplication.shared.keyWindow?.rootViewController else {
-            print("Error loading the rootViewController")
-            return
-        }
-
-        var leafViewController = rootViewController
-        while leafViewController.presentedViewController != nil && !leafViewController.presentedViewController!.isBeingDismissed {
-            leafViewController = leafViewController.presentedViewController!
-        }
-
-        leafViewController.present(self, animated: true, completion: nil)
-    }
 }
