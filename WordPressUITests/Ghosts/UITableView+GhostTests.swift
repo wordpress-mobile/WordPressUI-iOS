da58bb81f9ea2e0cdb1ec634588e0008cda7a047
Leandro Alonso <contato@leandroalonso.com>
2019-05-16 18:47:26 -0300

    Test new Ghost features

---
+++ WordPressUITests/Ghosts/UITableView+GhostTests.swift
@@ -0,0 +1,33 @@
+import XCTest
+
+@testable import WordPressUI
+
+class UITableViewGhostTests: XCTestCase {
+    func testCallWillStartGhostAnimationBeforeAnimating() {
+        let tableView = UITableView()
+        tableView.register(GhostMockCell.self, forCellReuseIdentifier: "ghost")
+        tableView.displayGhostContent(options: GhostOptions(reuseIdentifier: "ghost", rowsPerSection: [1]), style: .default)
+
+        tableView.dataSource?.tableView(tableView, cellForRowAt: IndexPath(row: 0, section: 0))
+        
+        XCTAssertTrue(GhostMockCell.willStartGhostAnimationCalled)
+    }
+
+    func testCellDoesntHaveToConformToGhostCellDelegate() {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.displayGhostContent(options: GhostOptions(reuseIdentifier: "cell", rowsPerSection: [1]), style: .default)
+
+        let cell = tableView.dataSource?.tableView(tableView, cellForRowAt: IndexPath(row: 0, section: 0))
+
+        XCTAssertNotNil(cell)
+    }
+}
+
+class GhostMockCell: UITableViewCell, GhostCellDelegate {
+    static var willStartGhostAnimationCalled = false
+
+    func willStartGhostAnimation() {
+        GhostMockCell.willStartGhostAnimationCalled = true
+    }
+}
