3039182dd1d1b72135f198a7f458ecac1f909ec5
Josh Heald <joshheald@users.noreply.github.com>
2021-12-10 08:45:19 +0000

    105 Update comment to match change history

--- WordPressUI/Extensions/NSBundle+ResourceBundle.swift
+++ WordPressUI/Extensions/NSBundle+ResourceBundle.swift
@@ -3,8 +3,8 @@ import Foundation
 extension Bundle {
     @objc public class var wordPressUIBundle: Bundle {
         let defaultBundle = Bundle(for: FancyAlertViewController.self)
-        // If installed with CocoaPods, resources will be in WordPressUI.bundle
         if let resourceBundle = Bundle(url: defaultBundle.bundleURL.appendingPathComponent("WordPressUIResources.bundle")) {
+        // If installed with CocoaPods, resources will be in WordPressUIResources.bundle
             return resourceBundle
         }
         // Otherwise, the default bundle is used for resources
